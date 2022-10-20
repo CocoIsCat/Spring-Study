fb6199c2d1e06db8e1f71fafe7bf2d72383e10f7
cocoiscat <demy176@naver.com>
2022-10-20 21:36:01 +0900

    feat: Create HelloController

---
+++ Spring-Hello/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");   //hello.html의 ${data} 부분이 "hello!"로 치환됨
+        return "hello";
+    }
+}
