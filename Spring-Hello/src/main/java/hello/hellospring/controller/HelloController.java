4b168c6a5d04b15fa027057a70b90be208824ffb
cocoiscat <demy176@naver.com>
2022-10-24 20:14:19 +0900

    feat: Develop hellomvc Controller

--- Spring-Hello/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring-Hello/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");   //hello.html의 ${data} 부분이 "hello!"로 치환됨
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-templates";
     }
 }
