ce6cf57a229f1c6f42f37459b6e034f8f23d3704
cocoiscat <demy176@naver.com>
2022-10-27 16:21:24 +0900

    feat: Create MemoryMemberRepository

---
+++ Spring-Hello/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;  //키값을 생성
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findByID(Long id) {
+        return Optional.ofNullable(store.get(id));  //optional로 감싸면 null값을 반환할 수 있음 : 클라이언트에서 할 수 있음
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //람다식 -> 이름이 같은 객체 찾아서 optional 값으로 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
