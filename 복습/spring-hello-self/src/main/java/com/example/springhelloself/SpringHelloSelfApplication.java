72621f1c2425d7497ae9d00c048d15ca78075eb4
cocoiscat <demy176@naver.com>
2022-12-02 14:20:50 +0900

    Initial Commit

---
+++ 복습/spring-hello-self/src/main/java/com/example/springhelloself/SpringHelloSelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhelloself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloSelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloSelfApplication.class, args);
+    }
+
+}
