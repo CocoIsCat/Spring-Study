58da3fe64e2c8fabd42a85de7cf6f98b837eb108
cocoiscat <demy176@naver.com>
2022-12-21 15:45:02 +0900

    feat: MemoryMemberRepository 생성
    MemberRepository 인터페이스를 구현하여 메모리에서 회원들을 관리함

---
+++ Spring-Introduction/복습/spring-hello-self/src/main/java/com/example/springhelloself/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package com.example.springhelloself.repository;
+
+import com.example.springhelloself.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+     
+    @Override
+    public Member save(Member member) {
+        store.put(++sequence, member);
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
