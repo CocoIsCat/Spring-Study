ac13377517e5d5898f42c0c29a25e1e367240a97
cocoiscat <demy176@naver.com>
2022-12-26 17:21:43 +0900

    fix: Map 지역변수로 변경

--- Spring-Introduction/복습/spring-hello-self/src/main/java/com/example/springhelloself/repository/MemoryMemberRepository.java
+++ Spring-Introduction/복습/spring-hello-self/src/main/java/com/example/springhelloself/repository/MemoryMemberRepository.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
      
     @Override
