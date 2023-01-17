14b5bc9dd7e04767e5792deb9011e592737322e0
cocoiscat <demy176@naver.com>
2023-01-17 15:14:24 +0900

    Initial Commit

---
+++ Spring Core/복습/core-self/src/test/java/hello/coreself/CoreSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.coreself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
