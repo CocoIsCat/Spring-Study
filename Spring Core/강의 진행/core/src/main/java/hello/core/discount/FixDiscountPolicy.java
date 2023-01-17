307e2277e28b0ac8b1bfc8fa75fc6785c311a04e
cocoiscat <demy176@naver.com>
2023-01-17 15:04:54 +0900

    스프링 핵심 원리 이해1 - 예제 만들기

---
+++ Spring Core/강의 진행/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmout = 1000;
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return 1000;
+        }
+        else {
+            return 0;
+        }
+    }
+}
